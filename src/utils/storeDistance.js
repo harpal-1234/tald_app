6a3ea82ad74d14a45505322558156910d618d23e
TarunDevstory <tarun@dev-story.com>
2022-12-28 19:16:46 +0530

    third commit

--- src/utils/storeDistance.js
+++ src/utils/storeDistance.js
@@ -9,69 +9,69 @@ const {
 const distance = require("google-distance-matrix");
 const config = require("../config/config");
 const FCM = require("fcm-node");
-const serverKey = config.fcmServerKey;
-const fcm = new FCM(serverKey);
+// const serverKey = config.fcmServerKey;
+// const fcm = new FCM(serverKey);
 
-const storeDistance = async (
-  storeId,
-  long,
-  lat,
-  userId,
-  type,
-  title,
-  image,
-  body,
-  matchId
-) => {
+// const storeDistance = async (
+//   storeId,
+//   long,
+//   lat,
+//   userId,
+//   type,
+//   title,
+//   image,
+//   body,
+//   matchId
+// ) => {
   
 
-  const user = await User.findOne({ _id: userId,isPushNotification:true, isDeleted: false });
+//   const user = await User.findOne({ _id: userId,isPushNotification:true, isDeleted: false });
 
-  if (!user) {
-    throw new OperationalError(
-      STATUS_CODES.ACTION_FAILED,
-      ERROR_MESSAGES.DOES_NOT_EXIST
-    );
+//   if (!user) {
+//     throw new OperationalError(
+//       STATUS_CODES.ACTION_FAILED,
+//       ERROR_MESSAGES.DOES_NOT_EXIST
+//     );
   
-  }
-  const token= await Token.find({user: user.id,isDeleted: false}).distinct("device.token").lean();
+//   }
+//   const token= await Token.find({user: user.id,isDeleted: false}).distinct("device.token").lean();
 
-  var message = {
-    registration_ids: token,
+//   var message = {
+//     registration_ids: token,
 
-    //collapse_key: 'your_collapse_key',
-    priority: "high",
-    content_available: true,
+//     //collapse_key: 'your_collapse_key',
+//     priority: "high",
+//     content_available: true,
 
-    notification: {
-      title: title,
-      body: body,
-      imageUrl: image,
-    },
+//     notification: {
+//       title: title,
+//       body: body,
+//       imageUrl: image,
+//     },
 
-    data: {
-      //you can send only notification or only data(or include both)
-      type: type,
-      match: matchId,
-    },
-  };
+//     data: {
+//       //you can send only notification or only data(or include both)
+//       type: type,
+//       match: matchId,
+//     },
+//   };
 
-  fcm.send(message, function (err, response) {
-    if (err) {
-      console.log("something went wrong");
-      throw new OperationalError(
-        STATUS_CODES.ACTION_FAILED,
-        ERROR_MESSAGES.DOES_NOT_EXIST
-      );
-    } else {
-      return response;
-    }
-  });
-};
+//   fcm.send(message, function (err, response) {
+//     if (err) {
+//       console.log("something went wrong");
+//       throw new OperationalError(
+//         STATUS_CODES.ACTION_FAILED,
+//         ERROR_MESSAGES.DOES_NOT_EXIST
+//       );
+//     } else {
+//       return response;
+//     }
+//   });
+// };
 
-module.exports = {
-  storeDistance,
-};
+// module.exports = {
+//   storeDistance,
+// };
 
 // const store=await Store.findOne({_id:storeId,isDeleted:false});
 // if(!store)
