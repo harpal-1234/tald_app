45cde02752d0293a1e43cf2fc8a4f00435511285
Sarvesh <sarvesh@dev-story.com>
2023-10-09 15:34:09 +0530

    commit

--- src/services/app.services.js/vendor.js
+++ src/services/app.services.js/vendor.js
@@ -347,7 +347,7 @@ export const consultationAction = async (
   return data;
 };
 export const zoom = async () => {
-  const redirect_uri = "https:loalhost:5050/vendor/app/callBack";
+  const redirect_uri = "https:api.tald.co/vendor/app/callBack";
   const clientId = "GuiPmdbXTwGFQRXnCvatKA";
   const authUrl = `https://zoom.us/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirect_uri}`;
   return authUrl;
