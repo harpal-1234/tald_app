b532bc572b9d65ecb30c6988eb321e94d3a1eca9
Harpal <harpal@Harpals-MacBook-Pro.local>
2023-11-07 11:46:42 +0530

    commit

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -152,7 +152,7 @@ console.log(socket.decoded.user,"llllllhihiohohhhhooioggogo")
         console.log(userCache[receiverId], senderId);
         console.log(message);
         if (message.isEmit) {
-          console.log(userCache[conversation._id])
+          console.log(userCache[conversation._id],"cccccccccccccccccc")
           userCache[conversation._id].forEach((users) => {
             if (users[receiverId]) {
               users[receiverId].forEach((socketId) => {
