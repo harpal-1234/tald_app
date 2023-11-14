912e9a9088dd51f25ddb30de6ba642cbd1675dc2
Harpal <harpal@Harpals-MacBook-Pro.local>
2023-11-14 21:16:03 +0530

    commit

--- src/services/user/auth.service.js
+++ src/services/user/auth.service.js
@@ -910,7 +910,7 @@ export const changePassword = async (userId, oldPassword, newPassword) => {
   let updatedPassword = { password: await bcrypt.hash(newPassword, 8) };
   //Object.assign(user, updatedPassword);
 
-  await user.findByIdAndUpdate(
+  await user.findOneAndUpdate(
     { _id: userId, isDeleted: false },
     { updatedPassword }
   );
